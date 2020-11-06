8fc4cdd0794402935f5ca059b57b1a38c23bf9fa
hys <heyisheng@shiyuegame.com>
2020-11-06 17:01:41 +0800

    ruby创建目录

--- rubygit.rb
+++ rubygit.rb
@@ -3,7 +3,7 @@
 # The Greeter class
 class Greeter
     def initialize(name)
-      @name = name.capitalize
+      @name = name.capitalize #不用像java那样定义
     end
   
     def salute
@@ -11,12 +11,29 @@ class Greeter
     end
   end
   
-  # Create a new object
-  g = Greeter.new("world")
-  
-  # Output "Hello World!"
-  g.salute
-  
 
-  Dir.mkdir(".git2")
+  # Create a new object
+  g = Greeter.new("world") # new会调用到initialize吧？initialize不写的话有默认吗？
   
+  Greeter.salute
+
+  # Output "Hello World!"
+  g.salute #用创建出来的对象g调用salute，类Greeter可以调用salute吗？
+  
+  # 判断是否存在
+  # 创建.git文件夹
+  # 进入.git
+  puts Dir.pwd 
+  if Dir.exist?(".git2") 
+    # 进去判断是不是空的，空的才操作
+    Dir.chdir( Dir.pwd + "/.git2")
+    Dir.mkdir("objects")
+    Dir.mkdir("hooks")
+    Dir.mkdir("info")
+    Dir.mkdir("refs")
+    puts Dir.pwd 
+  else
+    Dir.mkdir(".git2")
+    puts Dir.pwd 
+  end
+ 
